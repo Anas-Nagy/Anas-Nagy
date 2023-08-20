b9aa05e82c2995fe7222fa51892482c183ed0bca
Anas-Nagy <138384695+Anas-Nagy@users.noreply.github.com>
2023-08-20 22:28:31 +0000

    Mon, Aug 21, 2023, 12:28 AM +02:00

--- CS50_python/problem_set_2/Nutrition_Facts/nutrtion.py
+++ CS50_python/problem_set_2/Nutrition_Facts/nutrtion.py
@@ -1,10 +1,10 @@
-x = input("Item:").capitalize()
+x = input("Item:").capitalize().strip()
 food = {
       "Apple": 130, "Avocado": 50, "Banana": 110, "Cantaloupe": 50, "Grapefruit": 60, "Grapes": 90,
       "HoneydewMelon": 50, "Kiwifruit": 90, "Lemon": 15, "Lime": 15, "Nectarine": 60, "Orange": 80, "Peach": 60,
       "Pear": 100, "Pineapple": 50, "Plums": 70, "Strawberries": 50, "Tangerine": 50,  # This is line 18
       "Watermelon": 80 ,"Sweet Cherries": 100
   }
-
+print(x)
 if x in food:
      print(food[x])
